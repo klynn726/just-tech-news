3a1df915bc0527752dc8c1b0093d5e947259e0a1
klynn726 <49536087+klynn726@users.noreply.github.com>
2021-05-06 11:53:10 -0700

    lesson 3

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -35,10 +35,11 @@ router.get('/', (req, res) => {
         posts,
         loggedIn: req.session.loggedIn
       });
-    })
+   })
     .catch(err => {
       console.log(err);
       res.status(500).json(err);
+
     });
 });
 
